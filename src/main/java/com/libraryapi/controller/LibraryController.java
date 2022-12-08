055085bb477bbbc1e35b2bd84c4bf43c1ebc6027
Leo Villalobos <villaleobos@gmail.com>
2022-12-08 14:09:53 -0800

    HttpClientErrorException.NotFound -> ResponseStatusException

--- src/main/java/com/libraryapi/controller/LibraryController.java
+++ src/main/java/com/libraryapi/controller/LibraryController.java
@@ -44,12 +44,12 @@ public class LibraryController {
 	 * @param book_id The ID of the book to check out
 	 * @param patron_id The ID of the patron who wishes to check out the book
 	 * @return <code>0</code> if operation was Ok, <code>-3</code> if patron owes fines
-	 * @throws org.springframework.web.client.HttpClientErrorException.NotFound if either <code>book_id</code>
+	 * @throws ResponseStatusException if either <code>book_id</code>
 	 * or <code>patron_id</code> not found
 	 */
 	@PutMapping("/book/{book_id}/checkout/{patron_id}")
 	@Transactional
-	public int checkoutBook(@PathVariable int book_id, @PathVariable int patron_id) throws HttpClientErrorException.NotFound {
+	public int checkoutBook(@PathVariable int book_id, @PathVariable int patron_id) throws ResponseStatusException {
 		System.out.printf("\t[INFO] Endpoint /book/%d/checkout/%d hit!\n", book_id, patron_id);
 
 		var bookToCheckout = booksRepository.findByBookId(book_id);
@@ -82,11 +82,11 @@ public class LibraryController {
 	 * Returns a book that a patron had checked out
 	 * @param book_id The ID of the book to return
 	 * @return <code>0</code> if operation was Ok
-	 * @throws org.springframework.web.client.HttpClientErrorException.NotFound if <code>book_id</code> not found
+	 * @throws ResponseStatusException if <code>book_id</code> not found
 	 */
 	@PutMapping("/book/{book_id}/return")
 	@Transactional
-	public int returnBook(@PathVariable int book_id) throws HttpClientErrorException.NotFound {
+	public int returnBook(@PathVariable int book_id) throws ResponseStatusException {
 		System.out.printf("\t[INFO] Endpoint /book/%d/return hit!\n", book_id);
 
 		var bookToReturn = booksRepository.findByBookId(book_id);
@@ -107,11 +107,11 @@ public class LibraryController {
 	 * List a patron's checkouts
 	 * @param patron_id The ID of the patron
 	 * @return A list of <code>BookDTO</code>'s for each book checked out by the patron
-	 * @throws org.springframework.web.client.HttpClientErrorException.NotFound if <code>patron_id</code>
+	 * @throws ResponseStatusException if <code>patron_id</code>
 	 * not found
 	 */
 	@GetMapping("/patron/{patron_id}/checkouts")
-	public List<BooksDTO> listPatronCheckouts(@PathVariable int patron_id) throws HttpClientErrorException.NotFound {
+	public List<BooksDTO> listPatronCheckouts(@PathVariable int patron_id) throws ResponseStatusException {
 		System.out.printf("\t[INFO] Endpoint /patron/%d/checkouts hit!\n", patron_id);
 
 		var patron = patronsRepository.findByPatronId(patron_id);
@@ -137,11 +137,11 @@ public class LibraryController {
 	 * Check out if patron information
 	 * @param patron_id The ID of the patron to list
 	 * @return A <code>PatronDTO</code>, holding all the patron's data
-	 * @throws org.springframework.web.client.HttpClientErrorException.NotFound if <code>patron_id</code>
+	 * @throws ResponseStatusException if <code>patron_id</code>
 	 * not found
 	 */
 	@GetMapping("/patron/{patron_id}")
-	public PatronsDTO fetchPatron(@PathVariable int patron_id) throws HttpClientErrorException.NotFound  {
+	public PatronsDTO fetchPatron(@PathVariable int patron_id) throws ResponseStatusException  {
 		System.out.printf("\t[INFO] Endpoint /patron/%d hit!\n", patron_id);
 
 		var patron = patronsRepository.findByPatronId(patron_id);
